b229059928423737e8c72e229db4e5d7d22d8619
bairagidevendra30@gmail.com <bairagidevendra30@gmail.com>
2025-08-25 13:43:42 +0530

    Hey i just updatedthe code

--- index.ts
+++ index.ts
@@ -1,8 +1,3 @@
-let enum = 1;
-console.log(enum);
-export {};
-// Error: 'enum' is a reserved word and cannot be used as an identifier.
-// @filename: index.ts
-// Error: 'enum' is a reserved word and cannot be used as an identifier.
-let enum = 1;
-console.log(enum);
+function const [second] = 
+[1, 2, 3];
+console.log(second);
